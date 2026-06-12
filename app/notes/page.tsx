b801f78ebc6cad025ef34c50d417060d9fad0174
Маргарита Голуб <ritagolub1996@gmail.com>
2026-06-12 17:49:24 +0200

    Initial commit

---
+++ app/notes/page.tsx
@@ -0,0 +1,23 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import { fetchNotes } from '../../lib/api';
+import NotesClient from './Notes.client';
+
+export default async function NotesPage() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['notes', '', 1],
+    queryFn: () =>
+      fetchNotes({ keyword: '', currentPage: 1, itemsPerPage: 10 }),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient />
+    </HydrationBoundary>
+  );
+}
